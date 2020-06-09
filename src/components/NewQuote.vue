e81733f5481b74e41d420ba61ba84a9d28e7ab5d
Dhruvil Dave <dhruvil@gkmit.co>
2020-06-09 14:14:59 +0530

    added first commit

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea v-model="quote" rows="3" class="form-control"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    createNew(){
+      this.$emit('quoteAdded', this.quote);
+      this.quote='';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
